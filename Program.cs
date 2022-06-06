a6dd947af8665aa6a77f076b41c252b7a04ea6bd
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-06 13:13:15 +0300

    01 нарисовали две точки, используя класс Point

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Point p1 = new Point();
+p1.x = 1;
+p1.y = 3;
+p1.sym = '*';
+p1.Draw();
+
+Point p2 = new Point();
+p2.x = 4;
+p2.y = 5;
+p2.sym = '#';
+p2.Draw();
+
+class Point {
+	public int x;
+	public int y;
+	public char sym;
+
+	public void Draw() {
+		Console.SetCursorPosition(x, y);
+		Console.Write(sym);
+	}
+}
